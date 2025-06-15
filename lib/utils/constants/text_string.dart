1ede67985d654f9b80168c6ece0916fa08c59454
UnknownVisuals <reynaldig40@gmail.com>
2025-06-16 06:01:39 +0700

    setup project structure

---
+++ lib/utils/constants/text_string.dart
@@ -0,0 +1,65 @@
+/// This class contains all the App Text in String formats.
+class REYTexts {
+  // -- GLOBAL Texts
+  static const String and = "dan";
+  static const String skip = "Lewati";
+  static const String done = "Selesai";
+  static const String submit = "Kirim";
+  static const String appName = "Sobat Sampah";
+  static const String rContinue = "Lanjutkan";
+
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 =
+      "Kumpulkan Sampah,\nCintai Lingkungan!";
+  static const String onBoardingTitle2 = "Setor Sampah,\nRaih Manfaat!";
+  static const String onBoardingTitle3 = "Tukar Sampah,\nDapat Poin!";
+
+  static const String onBoardingSubTitle1 =
+      "Kumpulkan sampah di sekitarmu dan jadi pahlawan lingkungan.";
+  static const String onBoardingSubTitle2 =
+      "Setorkan sampah ke bank sampah terdekat dan lihat perubahan nyata.";
+  static const String onBoardingSubTitle3 =
+      "Tukarkan poin dengan barang-barang menarik yang kamu inginkan.";
+
+  // -- Authentication Forms
+  static const String firstName = "Nama Depan";
+  static const String lastName = "Nama Belakang";
+  static const String email = "Email";
+  static const String password = "Kata Sandi";
+  static const String newPassword = "Kata Sandi Baru";
+  static const String username = "Nama Pengguna";
+  static const String phoneNo = "Nomor Telepon";
+  static const String rememberMe = "Ingat Saya";
+  static const String forgetPassword = "Lupa Kata Sandi?";
+  static const String signIn = "Masuk";
+  static const String createAccount = "Buat Akun";
+  static const String orSignInWith = "atau masuk dengan";
+  static const String orSignUpWith = "atau daftar dengan";
+  static const String iAgreeTo = "Saya setuju dengan";
+  static const String privacyPolicy = "Kebijakan Privasi";
+  static const String termsOfUse = "Syarat Penggunaan";
+  static const String verificationCode = "Kode Verifikasi";
+  static const String resendEmail = "Kirim Ulang Email";
+  static const String resendEmailIn = "Kirim ulang email dalam";
+
+  // -- Authentication Headings
+  static const String loginTitle = "Selamat datang kembali,";
+  static const String loginSubTitle =
+      "Kumpulkan dan tukarkan sampahmu menjadi uang!";
+  static const String signupTitle = "Mari buat akun Anda";
+  static const String forgetPasswordTitle = "Lupa kata sandi";
+  static const String forgetPasswordSubTitle =
+      "Jangan khawatir, kadang orang bisa lupa juga, masukkan email Anda dan kami akan mengirimkan tautan reset kata sandi.";
+  static const String changeYourPasswordTitle =
+      "Email Reset Kata Sandi Terkirim";
+  static const String changeYourPasswordSubTitle =
+      "Keamanan Akun Anda adalah Prioritas Kami! Kami Telah Mengirimkan Tautan Aman untuk Mengubah Kata Sandi Anda dan Melindungi Akun Anda.";
+  static const String confirmEmail = "Verifikasi alamat email Anda!";
+  static const String confirmEmailSubTitle =
+      "Selamat! Akun Anda Menunggu: Verifikasi Email Anda untuk Mulai Berbelanja dan Nikmati Penawaran Tak Tertandingi serta Penawaran yang Dipersonalisasi.";
+  static const String emailNotReceivedMessage =
+      "Tidak menerima email? Periksa folder sampah/spam atau kirim ulang.";
+  static const String yourAccountCreatedTitle = "Akun Anda berhasil dibuat!";
+  static const String yourAccountCreatedSubTitle =
+      "Selamat Datang di Destinasi Belanja Utama Anda: Akun Anda Telah Dibuat, Nikmati Kegembiraan Belanja Online yang Lancar!";
+}
---
+++ lib/utils/popups/snackbar.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:lively/utils/constants/colors.dart';
+
+class REYLoaders {
+  static hideSnackBar() =>
+      ScaffoldMessenger.of(Get.context!).hideCurrentSnackBar();
+
+  static customToast({required message}) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(
+        elevation: 0,
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.transparent,
+        content: Container(
+          padding: const EdgeInsets.all(12.0),
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: REYColors.grey.withValues(alpha: 0.9),
+          ),
+          child: Center(
+            child: Text(
+              message,
+              style: Theme.of(Get.context!).textTheme.labelLarge,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static successSnackBar({required title, message = '', duration = 3}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: Colors.white,
+      backgroundColor: REYColors.primary,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: Duration(seconds: duration),
+      margin: const EdgeInsets.all(10),
+      icon: const Icon(Iconsax.check, color: REYColors.white),
+    );
+  }
+
+  static warningSnackBar({required title, message = ''}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: REYColors.white,
+      backgroundColor: Colors.orange,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: REYColors.white),
+    );
+  }
+
+  static errorSnackBar({required title, message = ''}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: REYColors.white,
+      backgroundColor: Colors.red.shade600,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: REYColors.white),
+    );
+  }
+}
